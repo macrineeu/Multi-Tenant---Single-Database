4db985dd8c842e61221de3b7e243b91e09809122
Matheus Macrineu <matheus.macrineu@connectcom.com.br>
2021-11-30 22:13:35 -0300

    add novo campo para registrar nome da empresa

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,10 +12,24 @@
                         @csrf
 
                         <div class="form-group row">
+                            <label for="tenant" class="col-md-4 col-form-label text-md-right">Empresa</label>
+
+                            <div class="col-md-6">
+                                <input id="tenant" type="text" class="form-control @error('tenant') is-invalid @enderror" name="tenant" value="{{ old('tenant') }}" required autocomplete="tenant" autofocus>
+
+                                @error('tenant')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
                             <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name">
 
                                 @error('name')
                                     <span class="invalid-feedback" role="alert">
