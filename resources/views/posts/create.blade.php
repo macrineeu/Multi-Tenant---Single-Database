971d23f97ed62b54972b6496c042eebbbafd35a3
Matheus Macrineu <matheus.macrineu@connectcom.com.br>
2021-11-30 23:07:16 -0300

    criando form para cadastrar post

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>Cadastrar Posts</h1>
+
+    <form action="{{ route('posts.store') }}" method="get">
+        <div class="form-group">
+            <input type="text" name="titulo" class="form-control" placeholder="Titulo">
+        </div>
+        <div class="form-group">
+            <textarea name="body" class="form-control" cols="30" rows="10" placeholder="Descricao"></textarea>
+        </div>
+        <div class="form-group">
+            <button type="submit" class="btn btn-sm btn-success">Cadastrar</button>
+        </div>
+    </form>
+
+@endsection
